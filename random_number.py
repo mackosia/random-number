9e81a074d6999e34f7cfa3b46f7b75b336794b2c
Maciej Kosiacki <105560819+mackosia@users.noreply.github.com>
2022-09-09 17:47:33 +0100

    Update random_number.py

--- random_number.py
+++ random_number.py
@@ -1,7 +1,10 @@
 from random import randint
-
-woof_meow = input("Type in meow or woof and get a random number!:")
-if"meow":
-    print(randint(1,100))
-else:
-    print(randint(1, 50))
+def random_number():
+    reply1 = input("Type in woof or meow:")
+    if reply1 == "woof":
+        print("Here is a random  number:"+str(randint(1,100)))
+    elif reply1 == "meow":
+        print("Here is a random  number but from 1-50:"+str(randint(1,5)))
+    else:
+        print("Not what I asked, try again")
+random_number()
